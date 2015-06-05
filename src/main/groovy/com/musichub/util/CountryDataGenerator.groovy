db695dca1fbd87c2b1a1fee1166281bcfbcd8f04
manufarfaro <manufarfaro@gmail.com>
2015-06-05 08:27:32 -0300

    #7: WIP. Country Data Class Generator added.

---
+++ src/main/groovy/com/musichub/util/CountryDataGenerator.groovy
@@ -0,0 +1,480 @@
+package com.musichub.util
+
+import com.musichub.Country
+
+class CountryDataGenerator {
+	public static void generate() {
+		Country countryAfghanistan = new Country(
+			iso: "AF",
+			name: "AFGHANISTAN",
+			nicename: "Afghanistan",
+			iso3: "AFG",
+			numCode: 4,
+			phoneCode: 93
+		).save(flush: true)
+		Country countryAlbania = new Country(
+			iso: "AL",
+			name: "ALBANIA",
+			nicename: "Albania",
+			iso3: "ALB",
+			numCode: 8,
+			phoneCode: 355
+		).save(flush: true)
+		Country countryArgelia = new Country(
+			iso: "DZ",
+			name: "ARGELIA",
+			nicename: "Argelia",
+			iso3: "DZA",
+			numCode: 12,
+			phoneCode: 213
+		).save(flush: true)
+		Country countryAntiguaBarbuda = new Country(
+			iso: "AG",
+			name: "ANTIGUA Y BARBUDA",
+			nicename: "Antigua y Barbuda",
+			iso3: "ATG",
+			numCode: 28,
+			phoneCode: 1268
+		).save(flush: true)
+		Country countryArgentina = new Country(
+			iso: "AR",
+			name: "ARGENTINA",
+			nicename: "Argentina",
+			iso3: "ARG",
+			numCode: 32,
+			phoneCode: 54
+		).save(flush: true)
+		Country countryArmenia = new Country(
+			iso: "AM",
+			name: "ARMENIA",
+			nicename: "Armenia",
+			iso3: "ARM",
+			numCode: 51,
+			phoneCode: 374
+		).save(flush: true)
+		Country countryAustralia = new Country(
+			iso: "AU",
+			name: "Australia",
+			nicename: "Australia",
+			iso3: "AUS",
+			numCode: 36,
+			phoneCode: 61
+		).save(flush: true)
+		Country countryAustria = new Country(
+			iso: "AT",
+			name: "AUSTRIA",
+			nicename: "Austria",
+			iso3: "AUT",
+			numCode: 40,
+			phoneCode: 43
+		).save(flush: true)
+		Country countryBelgica = new Country(
+			iso: "BE",
+			name: "BÉLGICA",
+			nicename: "Bélgica",
+			iso3: "BEL",
+			numCode: 56,
+			phoneCode: 32
+		).save(flush: true)
+		Country countryBolivia = new Country(
+			iso: "BO",
+			name: "BOLIVIA",
+			nicename: "Bolivia",
+			iso3: "BOL",
+			numCode: 68,
+			phoneCode: 591
+		).save(flush: true)
+		Country countryBosniaHerzegovina = new Country(
+			iso: "BA",
+			name: "BOSNIA Y HERZEGOVINA",
+			nicename: "Bosnia y Herzegovina",
+			iso3: "BIH",
+			numCode: 70,
+			phoneCode: 387
+		).save(flush: true)
+		Country countryBotswana = new Country(
+			iso: "BW",
+			name: "BOTSWANA",
+			nicename: "Botswana",
+			iso3: "BWA",
+			numCode: 72,
+			phoneCode: 267
+		).save(flush: true)
+		Country countryBrasil = new Country(
+			iso: "BR",
+			name: "BRASIL",
+			nicename: "Brasil",
+			iso3: "BRA",
+			numCode: 76,
+			phoneCode: 55
+		).save(flush: true)
+		Country countryCanada = new Country(
+			iso: "CA",
+			name: "CANADÁ",
+			nicename: "Canadá",
+			iso3: "CAN",
+			numCode: 124,
+			phoneCode: 1
+		).save(flush: true)
+		Country countryChile = new Country(
+			iso: "CL",
+			name: "CHILE",
+			nicename: "Chile",
+			iso3: "CHL",
+			numCode: 152,
+			phoneCode: 56
+		).save(flush: true)
+		Country countryChina = new Country(
+			iso: "CN",
+			name: "CHINA",
+			nicename: "China",
+			iso3: "CHN",
+			numCode: 156,
+			phoneCode: 86
+		).save(flush: true)
+		Country countryColombia = new Country(
+			iso: "CO",
+			name: "COLOMBIA",
+			nicename: "Colombia",
+			iso3: "COL",
+			numCode: 170,
+			phoneCode: 57
+		).save(flush: true)
+		Country countryCostaRica = new Country(
+			iso: "CR",
+			name: "COSTA RICA",
+			nicename: "Costa Rica",
+			iso3: "CRI",
+			numCode: 188,
+			phoneCode: 506
+		).save(flush: true)
+		Country countryCuba = new Country(
+			iso: "CU",
+			name: "CUBA",
+			nicename: "Cuba",
+			iso3: "CUB",
+			numCode: 192,
+			phoneCode: 53
+		).save(flush: true)
+		Country countryRepublicaDominicana = new Country(
+			iso: "DO",
+			name: "REPÚBLICA DOMINICANA",
+			nicename: "República Dominicana",
+			iso3: "DOM",
+			numCode: 214,
+			phoneCode: 1809
+		).save(flush: true)
+		Country countryEcuador = new Country(
+			iso: "EC",
+			name: "ECUADOR",
+			nicename: "Ecuador",
+			iso3: "ECU",
+			numCode: 218,
+			phoneCode: 593
+		).save(flush: true)
+		Country countryElSalvador = new Country(
+			iso: "SV",
+			name: "EL SALVADOR",
+			nicename: "El Salvador",
+			iso3: "SLV",
+			numCode: 222,
+			phoneCode: 503
+		).save(flush: true)
+		Country countryEspana = new Country(
+			iso: "ES",
+			name: "ESPAÑA",
+			nicename: "España",
+			iso3: "ESP",
+			numCode: 724,
+			phoneCode: 34
+		).save(flush: true)
+		Country countryEstadosUnidos = new Country(
+			iso: "US",
+			name: "ESTADOS UNIDOS",
+			nicename: "Estados Unidos",
+			iso3: "USA",
+			numCode: 840,
+			phoneCode: 1
+		).save(flush: true)
+		Country countryIslasMalvinas = new Country(
+			iso: "FK",
+			name: "ISLAS MALVINAS",
+			nicename: "Islas Malvinas",
+			iso3: "FLK",
+			numCode: 238,
+			phoneCode: 500
+		).save(flush: true)
+		Country countryGuatemala = new Country(
+			iso: "GT",
+			name: "GUATEMALA",
+			nicename: "Guatemala",
+			iso3: "GTM",
+			numCode: 320,
+			phoneCode: 502
+		).save(flush: true)
+		Country countryGuyana = new Country(
+			iso: "GY",
+			name: "GUYANA",
+			nicename: "Guyana",
+			iso3: "GUY",
+			numCode: 328,
+			phoneCode: 592
+		).save(flush: true)
+		Country countryHaiti = new Country(
+			iso: "HT",
+			name: "HAITÍ",
+			nicename: "Haití",
+			iso3: "HTI",
+			numCode: 332,
+			phoneCode: 509
+		).save(flush: true)
+		Country countryHolanda = new Country(
+			iso: "NL",
+			name: "HOLANDA",
+			nicename: "Holanda",
+			iso3: "NLD",
+			numCode: 528,
+			phoneCode: 31
+		).save(flush: true)
+		Country countryHonduras = new Country(
+			iso: "HN",
+			name: "HONDURAS",
+			nicename: "Honduras",
+			iso3: "HND",
+			numCode: 340,
+			phoneCode: 504
+		).save(flush: true)
+		Country countryHongKong = new Country(
+			iso: "HK",
+			name: "HONG KONG",
+			nicename: "Hong Kong",
+			iso3: "HKG",
+			numCode: 344,
+			phoneCode: 852
+		).save(flush: true)
+		Country countryIndia = new Country(
+			iso: "IN",
+			name: "INDIA",
+			nicename: "India",
+			iso3: "IND",
+			numCode: 356,
+			phoneCode: 91
+		).save(flush: true)
+		Country countryIndonesia = new Country(
+			iso: "ID",
+			name: "INDONESIA",
+			nicename: "Indonesia",
+			iso3: "IDN",
+			numCode: 360,
+			phoneCode: 62
+		).save(flush: true)
+		Country countryIran = new Country(
+			iso: "IR",
+			name: "IRÁN",
+			nicename: "Irán",
+			iso3: "IR",
+			numCode: 368,
+			phoneCode: 964
+		).save(flush: true)
+		Country countryIraq = new Country(
+			iso: "IQ",
+			name: "IRAQ",
+			nicename: "Iraq",
+			iso3: "IRQ",
+			numCode: 368,
+			phoneCode: 964
+		).save(flush: true)
+		Country countryIrlanda = new Country(
+			iso: "IE",
+			name: "IRLANDA",
+			nicename: "Irlanda",
+			iso3: "IRL",
+			numCode: 372,
+			phoneCode: 353
+		).save(flush: true)
+		Country countryIsrael = new Country(
+			iso: "IL",
+			name: "ISRAEL",
+			nicename: "Israel",
+			iso3: "ISL",
+			numCode: 376,
+			phoneCode: 972
+		).save(flush: true)
+		Country countryItalia = new Country(
+			iso: "IT",
+			name: "ITALIA",
+			nicename: "Italia",
+			iso3: "ITA",
+			numCode: 380,
+			phoneCode: 39
+		).save(flush: true)
+		Country countryJamaica = new Country(
+			iso: "JM",
+			name: "JAMAICA",
+			nicename: "Jamaica",
+			iso3: "JAM",
+			numCode: 388,
+			phoneCode: 1876
+		).save(flush: true)
+		Country countryJapon = new Country(
+			iso: "JP",
+			name: "JAPÓN",
+			nicename: "Japón",
+			iso3: "JPN",
+			numCode: 392,
+			phoneCode: 81
+		).save(flush: true)
+		Country countryCoreaNorte = new Country(
+			iso: "KP",
+			name: "COREA DEL NORTE",
+			nicename: "Corea del Norte",
+			iso3: "PKR",
+			numCode: 408,
+			phoneCode: 850
+		).save(flush: true)
+		Country countryCoreaSur = new Country(
+			iso: "KR",
+			name: "COREA DEL SUR",
+			nicename: "Corea del Sur",
+			iso3: "KOR",
+			numCode: 410,
+			phoneCode: 82
+		).save(flush: true)
+		Country countryMalasia = new Country(
+			iso: "MY",
+			name: "MALASIA",
+			nicename: "Malasia",
+			iso3: "MYS",
+			numCode: 458,
+			phoneCode: 60
+		).save(flush: true)
+		Country countryMexico = new Country(
+			iso: "MX",
+			name: "México",
+			nicename: "México",
+			iso3: "MEX",
+			numCode: 484,
+			phoneCode: 52
+		).save(flush: true)
+		Country countryNuevaZelanda = new Country(
+			iso: "NZ",
+			name: "Nueva Zelanda",
+			nicename: "Nueva Zelanda",
+			iso3: "NZL",
+			numCode: 554,
+			phoneCode: 64
+		).save(flush: true)
+		Country countryNicaragua = new Country(
+			iso: "NI",
+			name: "NICARAGUA",
+			nicename: "Nicaragua",
+			iso3: "NIC",
+			numCode: 558,
+			phoneCode: 505
+		).save(flush: true)
+		Country countryNigeria = new Country(
+			iso: "NG",
+			name: "NIGERIA",
+			nicename: "Nigeria",
+			iso3: "NGA",
+			numCode: 566,
+			phoneCode: 234
+		).save(flush: true)
+		Country countryPalestina = new Country(
+			iso: "PS",
+			name: "PALESTINA",
+			nicename: "Palestina",
+			iso3: null,
+			numCode: null,
+			phoneCode: 970
+		).save(flush: true)
+		Country countryPanama = new Country(
+			iso: "PA",
+			name: "PANAMA",
+			nicename: "Panama",
+			iso3: "PAN",
+			numCode: 591,
+			phoneCode: 507
+		).save(flush: true)
+		Country countryParaguay = new Country(
+			iso: "PY",
+			name: "PARAGUAY",
+			nicename: "Paraguay",
+			iso3: "PRY",
+			numCode: 600,
+			phoneCode: 595
+		).save(flush: true)
+		Country countryPeru = new Country(
+			iso: "PE",
+			name: "PERÚ",
+			nicename: "Perú",
+			iso3: "PER",
+			numCode: 604,
+			phoneCode: 51
+		).save(flush: true)
+		Country countryPolonia = new Country(
+			iso: "PL",
+			name: "POLONIA",
+			nicename: "Polonia",
+			iso3: "POL",
+			numCode: 616,
+			phoneCode: 48
+		).save(flush: true)
+		Country countryPortugal = new Country(
+			iso: "PT",
+			name: "PORTUGAL",
+			nicename: "Portugal",
+			iso3: "PRT",
+			numCode: 620,
+			phoneCode: 351
+		).save(flush: true)
+		Country countryPuertoRico = new Country(
+			iso: "PR",
+			name: "PUERTO RICO",
+			nicename: "Puerto Rico",
+			iso3: "PRI",
+			numCode: 630,
+			phoneCode: 1787
+		).save(flush: true)
+		Country countryReinoUnido = new Country(
+			iso: "GB",
+			name: "REINO UNIDO",
+			nicename: "Reino Unido",
+			iso3: "GBR",
+			numCode: 826,
+			phoneCode: 44
+		).save(flush: true)
+		Country countryRusia = new Country(
+			iso: "RU",
+			name: "RUSIA",
+			nicename: "Rusia",
+			iso3: "rus",
+			numCode: 643,
+			phoneCode: 70
+		).save(flush: true)
+		Country countryUcrania = new Country(
+			iso: "UA",
+			name: "UCRANIA",
+			nicename: "Ucrania",
+			iso3: "UKR",
+			numCode: 804,
+			phoneCode: 380
+		).save(flush: true)
+		Country countryUruguay = new Country(
+			iso: "UY",
+			name: "URUGUAY",
+			nicename: "Uruguay",
+			iso3: "URY",
+			numCode: 858,
+			phoneCode: 598
+		).save(flush: true)
+		Country countryVenezuela = new Country(
+			iso: "VE",
+			name: "VENEZUELA",
+			nicename: "Venezuela",
+			iso3: "VEN",
+			numCode: 862,
+			phoneCode: 58
+		).save(flush: true)
+	}
+}
